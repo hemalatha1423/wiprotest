32230536ffb672e7f470de7a630e418ae2e49f1f
hemal <hemal@LAPTOP-2AOH4R7M>
2025-08-06 19:25:52 +0530

    first commit

---
+++ thread/src/thread/number.java
@@ -0,0 +1,20 @@
+package thread;
+class number extends Thread {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            if (i == 5) {
+                try {
+                    Thread.sleep(500); // 500 milliseconds delay
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        number t = new number();
+        t.start();
+    }
+}
